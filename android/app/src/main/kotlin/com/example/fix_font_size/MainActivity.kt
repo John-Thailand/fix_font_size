60b5e480999e30a5712015d77265b26328f75a29
John-Thailand <hamoban69@gmail.com>
2022-04-30 13:08:22 +0900

    second commit

---
+++ android/app/src/main/kotlin/com/example/fix_font_size/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fix_font_size
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+          child: child!,
+        );
+      },
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App Bar'),
+        ),
+        body: const Center(
+          child: Text('Text'),
+        ),
+      ),
+    );
+  }
+}
